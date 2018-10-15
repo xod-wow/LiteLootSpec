fb6828ca28e1c369493075a2b56f3d96e26ffd04
Mike Battersby <mib@unimelb.edu.au>
2018-10-15 12:31:08 +1100

    Number don't work, key off the name. And instance I guess.

--- LiteLootSpec.lua
+++ LiteLootSpec.lua
@@ -13,11 +13,14 @@ local defaults = {
     specByNPC = { },
 }
 
--- God dammit Blizzard
-local function UnitDisplayName(unit)
-    local n, r = UnitFullName(unit)
-    r = r or select(2, UnitFullName("player"))
-    return format('%s-%s', n, r)
+local function GetBossInfoFromEJ()
+    if EncounterJournal and
+       EncounterJournal:IsVisible() and
+       EncounterJournal.encounterID then
+        local _, name = EJ_GetCreatureInfo(1)
+        local instance = select(6, EJ_GetEncounterInfo(EncounterJournal.encounterID))
+        return name, instance
+    end
 end
 
 local function GetUnitNPCID(unit)
@@ -26,23 +29,30 @@ local function GetUnitNPCID(unit)
 end
 
 local function GetLootSpecText(n)
-    local sex = UnitSex("player");
+    local sex = UnitSex('player')
+    local txt
     if n == 0 then
         local specIndex = GetSpecialization()
         local _, name = GetSpecializationInfo(specIndex, nil, nil, nil, sex);
-        return format(LOOT_SPECIALIZATION_DEFAULT, name);
+        txt = format(LOOT_SPECIALIZATION_DEFAULT, name)
     elseif n == nil then
-        return NONE
+        txt = NONE
     else
         local _, name = GetSpecializationInfoByID(n, sex)
-        return name or UNKNOWN
+        txt = name or UNKNOWN
     end
+    return YELLOW_FONT_COLOR_CODE .. txt .. FONT_COLOR_CODE_CLOSE
+end
+
+local function GetNPCText(name)
+    name = name or NONE
+    return YELLOW_FONT_COLOR_CODE .. name .. FONT_COLOR_CODE_CLOSE
 end
 
 function LiteLootSpec:Print(...)
     local cur = DEFAULT_CHAT_FRAME
     for i = 1,NUM_CHAT_WINDOWS do
-        local f = _G["ChatFrame"..i]
+        local f = _G['ChatFrame'..i]
         if f and f:IsVisible() then
             cur = f
             break
@@ -81,11 +91,14 @@ function LiteLootSpec:ApplyWantedSpec()
 end
 
 function LiteLootSpec:PLAYER_LOGIN()
-    local key = format('%s-%s', UnitFullName('player'))
+    local key = UnitGUID('player')
+
     LiteLootSpecDB = LiteLootSpecDB or { }
-    LiteLootSpecDB[key] = LiteLootSpecDB[key] or  CopyTable(defaults)
+    LiteLootSpecDB[key] = LiteLootSpecDB[key] or CopyTable(defaults)
 
     self.db = LiteLootSpecDB[key]
+
+    self.db.info = { UnitFullName('player') }
 
     self.wantedLootSpec = nil
     -- Is this necessary? PLAYER_LOOT_SPEC_UPDATED is probably fired early
@@ -101,13 +114,12 @@ function LiteLootSpec:PLAYER_TARGET_CHANGED()
         return
     end
 
-    local npcID = GetUnitNPCID('target')
-    if not npcID then
-        return
-    end
+    local npcName = UnitName('target')
+    local instance = select(8, GetInstanceInfo())
 
-    if self.db.specByNPC[npcID] then
-        self.wantedLootSpec = self.db.specByNPC[npcID]
+    if self.db.specByNPC[npcName] and
+       self.db.specByNPC[npcName].instance == instance then
+        self.wantedLootSpec = self.db.specByNPC[npcName].spec
     else
         self.wantedLootSpec = nil
     end
@@ -143,41 +155,63 @@ local function ParseSpecArg(arg)
     end
 end
 
+local function GetBossInfo()
+    local npc, instance = GetBossInfoFromEJ()
+    if not npc then
+        npc = UnitName('target')
+        instance = select(8, GetInstanceInfo())
+    end
+    return npc, instance
+end
+
 function LiteLootSpec:SlashCommandHandler(argstr)
-    local cmd, arg1, arg2 = strsplit(' ', strlower(argstr))
+    local cmd, arg1 = strsplit(' ', strlower(argstr))
     if cmd == 'list' then
-        for npc, spec in pairs(self.db.specByNPC) do
-            self:Print('%s -> %d (%s)', npc, spec, GetLootSpecText(spec))
+        for npc, info in pairs(self.db.specByNPC) do
+            self:Print('%s (instance %d) -> %d (%s)',
+                   GetNPCText(npc),
+                   info.instance,
+                   info.spec,
+                   GetLootSpecText(info.spec)
+                )
         end
     elseif cmd == 'target' then
-        local npc = GetUnitNPCID('target')
-        self:Print('LiteLootSpec: target npc = %s', tostring(npc))
+        local npc, instance = GetBossInfo()
+        self:Print('Target npc = %s (instance %s)',
+                GetNPCText(npc),
+                tostring(instance)
+            )
     elseif cmd == 'clear' then
-        local npc = arg1 or GetUnitNPCID('target')
+        local npc
+        if arg1 then
+            npc = argstr:match('^'..cmd..'%s+(.*)')
+        else
+            npc = GetBossInfo()
+        end
         if npc then
             self.db.specByNPC[npc] = nil
-            self:Print('LiteLootSpec: clearing spec for npc %d', npc)
+            self:Print('Clearing spec for npc %s', GetNPCText(npc))
         end
-    elseif cmd == 'set' then
-        local npc, spec
-        if arg2 then
-            npc = arg1
-            spec = ParseSpecArg(arg2)
+    elseif cmd == 'set' and arg1 then
+        local npc, instance = GetBossInfo()
+        local spec = ParseSpecArg(arg1)
+        if npc and spec then
+            self:Print('Setting spec %d (%s) for npc %s',
+                    spec,
+                    GetLootSpecText(spec),
+                    GetNPCText(npc)
+                )
+            self.db.specByNPC[npc] = { instance = instance, spec = spec }
         else
-            npc = GetUnitNPCID('target')
-            spec = ParseSpecArg(arg1)
-        end
-        if npc then
-            self:Print('Setting spec %s for npc %d', GetLootSpecText(spec), npc)
-            self.db.specByNPC[npc] = spec
+            self:Print('Target a boss or have it open in the Encounter Journal.')
         end
     elseif cmd == 'wipe' then
         wipe(self.db.specByNPC)
     else
         self:Print('Usage:')
         self:Print('  /ls list')
-        self:Print('  /ls set [<npcid>] <spec>')
-        self:Print('  /ls clear [<npcid>]')
+        self:Print('  /ls set <spec>')
+        self:Print('  /ls clear [<name>]')
         self:Print('  /ls wipe')
         self:Print('  /ls target')
     end
